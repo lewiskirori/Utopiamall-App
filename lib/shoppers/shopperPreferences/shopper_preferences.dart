f29017c0a90b062a4b5b7d2e8b04afe19353ff68
lewiskirori <amicableycot@gmail.com>
2023-11-06 08:35:06 +0300

    accessing its properties

--- lib/shoppers/shopperPreferences/shopper_preferences.dart
+++ lib/shoppers/shopperPreferences/shopper_preferences.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:utopiamall/shoppers/model/shopper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -6,5 +8,7 @@ class RememberShopperPrefs {
   // Remember shopper info
   static Future<void> storeRememberShopper(Shopper shopperInfo) async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
+    String shopperJsonData = jsonEncode(shopperInfo.toJson());
+    await preferences.setString("currentShopper", shopperJsonData);
   }
 }
