9ca342615745ac8873d06a886e63eeccf65c9c17
lewiskirori <amicableycot@gmail.com>
2023-11-09 21:55:08 +0300

    property access

--- lib/api_conn/api_conn.dart
+++ lib/api_conn/api_conn.dart
@@ -1,5 +1,5 @@
 class API {
-  static const hostConnect = "https://dce7-93-190-141-59.ngrok-free.app/utopiamall_api";
+  static const hostConnect = "https://855b-37-19-221-241.ngrok-free.app/utopiamall_api";
   static const hostConnectShopper = "$hostConnect/shopper";
 
   // Sign up shopper
