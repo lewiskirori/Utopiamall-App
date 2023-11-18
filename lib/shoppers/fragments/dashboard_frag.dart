e72eb5d7339c40db1ffa793f63028e9b5c139628
lewiskirori <amicableycot@gmail.com>
2023-11-18 04:09:04 +0300

    adjust layout requirements

--- lib/shoppers/fragments/dashboard_frag.dart
+++ lib/shoppers/fragments/dashboard_frag.dart
@@ -2,13 +2,32 @@ import 'package:flutter/material.dart';
 
 
 class DashboardFrag extends StatelessWidget {
+  String getGreeting() {
+    final hour = DateTime.now().hour;
+
+    if (hour < 4) {
+      return "Good Night";
+    } else if (hour < 12) {
+      return "Good Morning";
+    } else if (hour < 16) {
+      return "Good Afternoon";
+    } else {
+      return "Good Evening";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Utopia Dashboard"),
+        title: Text(getGreeting()),
+      ),
+      body: Center(
+        child: Text(
+          "Welcome to Your U-Dash!",
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
-}
+} 
