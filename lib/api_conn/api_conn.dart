403930db236b511678b7b8664a9ddc8db60109f2
lewiskirori <amicableycot@gmail.com>
2023-12-05 14:03:34 +0300

    granular control

--- lib/api_conn/api_conn.dart
+++ lib/api_conn/api_conn.dart
@@ -1,5 +1,5 @@
 class API {
-  static const hostConnect = "https://4bb6-93-190-141-60.ngrok-free.app/utopiamall_api";
+  static const hostConnect = "https://3871-93-190-141-60.ngrok-free.app/utopiamall_api";
   static const hostConnectShopper = "$hostConnect/shopper";
 
   // Sign up shopper
